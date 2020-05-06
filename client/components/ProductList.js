13635373459bef659b662231c9f8b9e5324bb368
Eric Tang <jivesushi@yahoo.com>
2020-05-06 12:11:34 -0400

    add ProductList component in MainContainer

---
+++ client/components/ProductList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <h2>
+      Product List
+    </h2>
+  );
+}
+ 
+export default Header;
--- client/containers/MainContainer.js
+++ client/containers/MainContainer.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import Header from '../components/Header';
+import ProductList from '../components/ProductList';
+import ProductDisplay from '../components/ProductDisplay';
 
 const MainContainer = () => {
   return (
