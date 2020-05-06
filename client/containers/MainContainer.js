ebcff97507c1bc2e75ef444fe24aca40bc723c16
Eric Tang <jivesushi@yahoo.com>
2020-05-06 12:09:17 -0400

    add Header component in MainContainer

--- client/containers/MainContainer.js
+++ client/containers/MainContainer.js
@@ -3,6 +3,9 @@ import React from 'react';
 const MainContainer = () => {
   return (
     <div>
+      <Header />
+      <ProductList />
+      <ProductDisplay />
       MainContainer
     </div>
   );
