14d0161087b44e803b0198dd7ae6907a8e91b379
Eric Tang <jivesushi@yahoo.com>
2020-05-06 20:22:19 -0400

    changed LoginForm to functional component and fixed error in displaying main page after successful login

--- client/containers/LoginContainer.js
+++ client/containers/LoginContainer.js
@@ -1,18 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Auth from '../components/Auth';
 import LoginForm from '../components/LoginForm';
 
 const LoginContainer = (props) => {
-  const handleLogin = () => {
-    Auth.login(() => {
-      props.history.push("/");
-    });
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const handleChangeUsername = e => {
+    setUsername(e.target.value.trim());
   };
-  
+  const handleChangePassword = e => {
+    setPassword(e.target.value.trim());
+  };
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log('username:' + username)
+    fetch('/auth/login', {
+        method: 'POST',
+        body: JSON.stringify({username, password}),
+        headers: { 'Content-Type': 'application/json' },
+    })
+        // 401 and 406  
+        .then(res => {
+            if (res.status === 401) {
+                setError('This password does not match')
+            } else if (res.status === 406) {
+                setError('This username is not found')
+                props.history.push('/login')
+            } else {
+              Auth.login(() => props.history.push('/'));
+            }
+        })
+  }
+
   return (
     <div>
-      <button onClick={handleLogin}>Log in</button>
-      <LoginForm />
+      <LoginForm handleSubmit={handleSubmit} handleChangeUsername={handleChangeUsername} handleChangePassword={handleChangePassword} error={error} />
     </div>
   );
 }
